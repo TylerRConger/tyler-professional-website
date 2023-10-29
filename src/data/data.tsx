ed607328d47c515ae3a07f063b20349ef051cede
unknown <TylerConger1998@gmail.com>
2023-10-29 14:04:42 -0500

    Update appropriately for links

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -182,8 +182,8 @@ export const skills: SkillGroup[] = [
         level: 8,
       },
       {
-        name: 'Humor',
-        level: 2,
+        name: 'Communication',
+        level: 9,
       }
     ]
   }
@@ -208,19 +208,19 @@ export const portfolioItems: PortfolioItem[] = [
   {
     title: 'Chatbot Poster',
     description: 'Analysis of some of my work in studying what types of language users prefer when interacting with chatbots. Studied specifically human curated text vs AI tweaked responses to understand what linguistic characteristics are prefered by end users.',
-    url: 'tyler-professional-website/Chatbot User Exerpience Symposium Poster 4.17.2020.pdf',
+    url: 'Chatbot User Exerpience Symposium Poster 4.17.2020.pdf',
     image: chatBotImage,
   },
    {
     title: 'Network Stength Project',
     description: 'Research paper where signals ability to propagate through various different building materials. Testing various networking protocols as well as different commonly used household building materials in an effort to study what materials may be better or limiting to wireless networking activity.',
-    url: 'https://tylerrconger.github.io/tyler-professional-website/Effects%20of%20Common%20Building%20Construction%20Materials%20on%20Signal.pdf',
+    url: 'Effects%20of%20Common%20Building%20Construction%20Materials%20on%20Signal.pdf',
     image: networkImage,
   },
    {
     title: 'Survey of Machine Learning in Image Processing',
     description: 'An overview of the ways different machine learning algorithms are used in image processing, covering topics of OCR, Autonomous Vehicles, and Facial Recognition. High level coverage of different alogrithms and there comperable performance as well as what is used in individual situations with challenges of each.',
-    url: 'tyler-professional-website/Machine Learning in Image Processing Survey.pdf',
+    url: 'Machine Learning in Image Processing Survey.pdf',
     image: MLSelfDrivingImage,
   },
   {
