bc5d5e845f751202efcab7dff757c0e2270da073
unknown <TylerConger1998@gmail.com>
2023-10-29 14:46:36 -0500

    Slow load image

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -8,7 +8,7 @@ import Section from '../Layout/Section';
 import Socials from '../Socials';
 
 const Hero: FC = memo(() => {
-  const {imageSrc, name, description, actions} = heroData;
+  const {imageSrc, profileImageSrc, name, description, actions} = heroData;
 
   return (
     <Section noPadding sectionId={SectionId.Hero}>
@@ -24,6 +24,14 @@ const Hero: FC = memo(() => {
           <div className="flex flex-col items-center gap-y-6 rounded-xl bg-gray-800/40 p-6 text-center shadow-lg backdrop-blur-sm">
             <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl">{name}</h1>
             {description}
+            <div className="relative h-36 w-24 overflow-hidden rounded-xl md:h-36 md:w-32">
+              <Image 
+              alt="about-me-image" 
+              className="transition-opacity opacity-0 duration-[2s] h-full w-full object-cover" 
+              src={profileImageSrc} 
+              onLoadingComplete={(image) => image.classList.remove("opacity-0")}
+              />
+            </div>
             <div className="flex gap-x-4 text-neutral-100">
               <Socials />
             </div>
